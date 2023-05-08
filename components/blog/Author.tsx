9b4bf2baad0b4da6ccb3cbdebeae12d101f20f3f
cristianvj <cristiandavid.vj@gmail.com>
2023-05-08 15:40:05 -0500

    terst to netlify rebuild

--- components/blog/Author.tsx
+++ components/blog/Author.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import React from 'react';
 import { FC } from 'react';
 import { Author } from '../../interfaces';
@@ -11,11 +10,8 @@ const Author: FC<Props> = ({ author }) => {
   return (
     <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-orange-600 bg-opacity-80'>
       <div className='absolute left-0 right-0 -top-14'>
-        <Image
+        <img
           alt={author.name}
-          unoptimized
-          height='100px'
-          width='100px'
           className='align-middle rounded-full'
           src={author.photo.url}
         />
